befd8590e33462e502dad7c792dea1dcef6fd04d
Jessica <0628222@schoolsnet.act.edu.au>
2023-02-15 09:32:35 +1100

    finally enemy die

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,9 +8,10 @@ func _ready():
 	
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
-		var bulletInstance = bulletSource.instance()
-		bulletInstance.position = Vector2(position.x, position.y-20)
-		get_tree().get_root().add_child(bulletInstance)
+		if GlobalVariables.bulletInstanceCount < 3:
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().get_root().add_child(bulletInstance)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
