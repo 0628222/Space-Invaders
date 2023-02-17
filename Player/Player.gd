e9a0cbfc621786f1743c54b675933cf84888926f
Jessica <0628222@schoolsnet.act.edu.au>
2023-02-17 12:33:25 +1100

    he he ha

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,10 +15,14 @@ func _process(delta):
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
-		move_and_collide(Vector2(-movement_speed * delta, 0))
+		if position.x > 10:
+			move_and_collide(Vector2(-movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_right"):
-		move_and_collide(Vector2(movement_speed * delta, 0))
+		if position.x < 1280:
+			move_and_collide(Vector2(movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_up"):
-		move_and_collide(Vector2( 0, -movement_speed * delta))
+		if position.y > 0:
+			move_and_collide(Vector2( 0, -movement_speed * delta))
 	if Input.is_action_pressed("ui_down"):
-		move_and_collide(Vector2( 0, movement_speed * delta))
+		if position.y < 1280:
+			move_and_collide(Vector2( 0, movement_speed * delta))
