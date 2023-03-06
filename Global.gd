bf1cbafd79c25823a8fb749cb4432c67655b09e3
Jessica <0628222@schoolsnet.act.edu.au>
2023-03-06 14:44:05 +1100

    high score fail

--- Global.gd
+++ Global.gd
@@ -4,3 +4,10 @@ var bulletInstanceCount = 0
 var enemyBulletInstanceCount = 0 
 
 var automaticFiring = false
+
+var scoringInformation = {
+	"currentScore": 0,
+	"currentPlayer": "User", 
+	"highScore": 0, 
+	"highScorePlayersName" : "Winner"
+}
--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -5,14 +5,17 @@ var currentTimer
 
 
 func _ready():
+	set_process(true)
 	currentTimer = countdownMax
 	$HUD/Countdown.text = str(currentTimer)
 
 	while currentTimer > 0:
 		yield(get_tree().create_timer(1.0), "timeout")
 		$HUD/Countdown.text = str(currentTimer)
-		currentTimer = currentTimer - 1
+		currentTimer = currentTimer - 1 
 		print(currentTimer)
-	print("Game Over")
-	get_tree().change_scene("res://Menu/Menu.tscn")
+		print("Game Over")
+		get_tree().change_scene("res://Menu/Menu.tscn")
 
+func _process(delta):
+	$HUD/Countdown.text = str(GlobalVariables.scoringInformation["currentScore"])
