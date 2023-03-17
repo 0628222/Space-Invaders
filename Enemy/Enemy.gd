63f332e536aa8c48c87ebceac469d491a22d7a1f
Jessica <0628222@schoolsnet.act.edu.au>
2023-03-17 12:51:26 +1100

    enemy move work

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,18 +1,16 @@
 extends KinematicBody2D
 
 func _ready():
-	print("enemy ready")
 	$Area2D.connect("area_entered", self, "_colliding")
 
 
 func _colliding(area):
-	print("here")
 	if area.is_in_group("right"):
 		get_parent().global_position.y += 10
 		get_parent().speed = get_parent().speed * -1
 	if area.is_in_group("left"):
 		get_parent().global_position.y += 10
-		get_parent().speed = get_parent().speed * -1
+		get_parent().speed =get_parent().speed * -1
 
 var bullet = preload("res://Bullet-Enemy/Bullet-Enemy.tscn")
 
