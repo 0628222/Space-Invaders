ef27b88eb11c38ad88e78fc3a7d2467c2d5964ca
Peter <0628222@schoolsnet.act.edu.au>
2023-05-15 14:43:19 +1000

    Background Changes

--- Button.gd
+++ Button.gd
@@ -1,10 +1,5 @@
 extends Button
 
-onready var anim = $AnimationPlayer
-func _ready():
-	anim.play("SpecialEnemyGIF")
-
-
 
 func _on_Layout_pressed():
 	get_tree().change_scene("res://Menu/Menu.tscn")
---
+++ SpecialEnemy.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+
+onready var anim = $AnimationPlayer
+
+func _ready():
+	anim.play("SpecialEnemyGIF")
+
+func _colliding(area):
+	if area.is_in_group("right"):
+		get_parent().global_position.y += 10
+		get_parent().speed = get_parent().speed * -1
+	if area.is_in_group("left"):
+		get_parent().global_position.y += 10
+		get_parent().speed =get_parent().speed * -1
+
+var bullet = preload("res://Bullet-Enemy/Bullet-Enemy.tscn")
+
+func _process(delta):
+#	while (true):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var my_random_number = rng.randf_range(2.0, 30.0)
+	#print("time: ",my_random_number)
+	yield(get_tree().create_timer(my_random_number), "timeout")
+	if GlobalVariables.enemyBulletInstanceCount < 5:
+		var bulletInstance = bullet.instance()
+		
+		bulletInstance.position = Vector2(global_position.x, global_position.y+20)
+		get_tree().get_root().add_child(bulletInstance)
